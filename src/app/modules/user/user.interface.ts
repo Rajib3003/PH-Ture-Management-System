fb7f59679e5a6f52e79bf685684adc5eb5ed47eb
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-09-15 18:02:25 +0600

    part-1 user interface create

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,36 @@
+import { Types } from "mongoose";
+
+export enum Role {
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN",
+    USER = "USER",
+    GUIDE = "GUIDE"
+}
+
+export enum isActived {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    BLOCKED = "BLOCKED"
+}
+
+export interface IAuthProvider {
+    provider : string; //Google , credential(email password)
+    providerId : string;
+}
+
+
+export interface IUser {
+    name : string;
+    email : string ;
+    password ?: string; 
+    phone ?: string;
+    picture ?: string;
+    address ?: string;
+    isDeleted ?: string;
+    isActived ?: isActived;
+    isVerified ?: string;
+    role : Role;
+    auths : IAuthProvider[];
+    booking ?: Types.ObjectId[];
+    guides ?: Types.ObjectId[];
+}
