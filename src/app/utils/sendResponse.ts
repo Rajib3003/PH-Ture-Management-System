40e476719220d6af95ef93e616863b30acbba674
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-09-23 12:40:15 +0600

    part 1 finally done

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,27 @@
+import { Response } from "express";
+
+
+interface TMeta {
+    total : number;
+};
+
+interface TResponse<T>{
+    success: boolean;
+    message: string;
+    statusCode: number;
+    data: T;
+    meta?: TMeta;
+}
+
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+    res.status(data.statusCode).json({
+        success : data.success,
+        message: data.message,
+        statuscode: data.statusCode,
+        meta: data.meta,
+        data: data.data,
+        
+    })
+}
+
+export default sendResponse;
