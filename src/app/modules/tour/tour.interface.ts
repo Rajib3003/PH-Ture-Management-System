66efeda7646e45d434a80edd8e74f67088931c78
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-10-08 14:52:27 +0600

    part 4 complet

---
+++ src/app/modules/tour/tour.interface.ts
@@ -0,0 +1,25 @@
+import { Types } from "mongoose";
+
+export interface ITourType {
+    name: string;
+
+}
+
+export interface ITour {
+    title: string;
+    slug: string;
+    description?: string;
+    image?: string;
+    location?: string;
+    costFrom?: number;
+    startDate?: Date;
+    endDate?: Date;
+    included?: string[];
+    excluded?: string[];
+    amenities?: string[];
+    turePlan?: string[];
+    maxGuest?: number;
+    minAge?: number;
+    division: Types.ObjectId;
+    tourType: Types.ObjectId; 
+}
