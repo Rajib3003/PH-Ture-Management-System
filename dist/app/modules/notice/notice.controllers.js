49db3d259bcfeaf7b0f6b928fa60d881e50d2e75
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-11-24 17:39:37 +0600

    notice image add

--- dist/app/modules/notice/notice.controllers.js
+++ dist/app/modules/notice/notice.controllers.js
@@ -19,7 +19,7 @@ const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
 const sendResponse_1 = __importDefault(require("../../utils/sendResponse"));
 const notice_service_1 = require("./notice.service");
 const createNotice = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const payload = req.body;
+    const payload = Object.assign(Object.assign({}, req.body), { images: req.files.map(file => file.path) });
     const result = yield notice_service_1.NoticeService.createNotice(payload);
     (0, sendResponse_1.default)(res, {
         success: true,
@@ -28,8 +28,98 @@ const createNotice = (0, catchAsync_1.default)((req, res, next) => __awaiter(voi
         data: result,
     });
 }));
+const getAllNotices = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const query = req.query;
+    const result = yield notice_service_1.NoticeService.getAllNotices(query);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_codes_1.default.OK,
+        success: true,
+        message: 'Notice retrieved successfully',
+        data: result.data,
+        meta: result.meta,
+    });
+}));
+const getSingleNotice = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const slug = req.params.slug;
+    const result = yield notice_service_1.NoticeService.getSingleNotice(slug);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Single Notice get successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result.data
+    });
+}));
+const updateNotice = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const noticeId = req.params.id;
+    const payload = Object.assign(Object.assign({}, req.body), { images: req.files.map(file => file.path) });
+    const result = yield notice_service_1.NoticeService.updateNotice(noticeId, payload);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Notice updated successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result
+    });
+}));
+const deleteNotice = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const noticeId = req.params.id;
+    const result = yield notice_service_1.NoticeService.deleteNotice(noticeId);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Notice delete successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result
+    });
+}));
+const getAllNoticeTypes = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield notice_service_1.NoticeService.getAllNoticeTypes();
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "All Notice Types get successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result,
+    });
+}));
+const createNoticeType = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    const result = yield notice_service_1.NoticeService.createNoticeType(name);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Notice Types successfully!!",
+        statusCode: http_status_codes_1.default.CREATED,
+        data: result,
+    });
+}));
+const updateNoticeType = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const noticeTypesId = req.params.id;
+    const payload = req.body;
+    const result = yield notice_service_1.NoticeService.updateNoticeType(noticeTypesId, payload);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Notice Type updated successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result,
+    });
+}));
+const deleteNoticeType = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const noticeTypesId = req.params.id;
+    const result = yield notice_service_1.NoticeService.deleteNoticeType(noticeTypesId);
+    (0, sendResponse_1.default)(res, {
+        success: true,
+        message: "Notice Type deleted successfully!!",
+        statusCode: http_status_codes_1.default.OK,
+        data: result,
+    });
+}));
 exports.NoticeController = {
-    createNotice
+    createNotice,
+    getAllNotices,
+    getSingleNotice,
+    updateNotice,
+    deleteNotice,
+    getAllNoticeTypes,
+    createNoticeType,
+    updateNoticeType,
+    deleteNoticeType,
 };
 // noticeRouters.post("/", async (req: Request, res: Response) => {
 //   try {
--- src/app/modules/notice/notice.models.ts
+++ src/app/modules/notice/notice.models.ts
@@ -1,19 +1,69 @@
 import { model, Schema } from "mongoose";
-import { INotice } from "./notice.interfaces";
+import { INotice, INoticeType } from "./notice.interfaces";
+
+
+const noticeTypeSchema = new Schema<INoticeType>({
+    name: { type: String, required: true, unique: true }
+}, {
+    timestamps: true,
+    versionKey: false
+})
+
+
+export const NoticeType = model<INoticeType>("TourType", noticeTypeSchema)
 
 
 
-const NoticeSchema: Schema = new Schema({
+const noticeSchema = new Schema<INotice>({
   title: { type: String, required: true },
+  slug: { type: String, unique: true },
   date: { type: Date, required: true },
   description: { type: String },
-  link: { type: String },
-  photo: { type: String }
+  images: { type: [String], default: [] },
+  noticeType: {
+        type: Schema.Types.ObjectId,
+        ref: "NoticeType",
+        required: true
+    }
 }, {
     timestamps: true,
     versionKey: false, 
 });
 
 
+noticeSchema.pre("save", async function(next){
+  if(this.isModified("title")){
+    const baseSlug = this.title.toLowerCase().split(" ").join("-")
+    let slug= `${baseSlug}`
 
-export const Notice = model<INotice>('Notice', NoticeSchema);
+    let counter = 0;
+
+    while (await Notice.exists({slug})){
+      slug = `${slug}-${counter++}`
+    }
+    this.slug = slug;
+  }
+  next()
+})
+
+noticeSchema.pre("findOneAndUpdate", async function (next){
+  const notice = this.getUpdate() as Partial<INotice>
+
+  if(notice.title){
+    const baseSlug = notice.title.toLowerCase().split(" ").join("-")
+    let slug = `${baseSlug}`
+
+    let counter = 0;
+
+    while (await Notice.exists({ slug })){
+      slug = `${slug}-${counter++}`
+    }
+    notice.slug = slug
+  }
+  this.setUpdate(notice)
+  next()
+})
+
+
+
+export const Notice = model<INotice>('Notice', noticeSchema);
