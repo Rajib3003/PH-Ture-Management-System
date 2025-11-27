39a438681232b46cc19361462554b95ae5190cbf
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-11-27 18:06:06 +0600

    limit add

--- dist/app/utils/QueryBuilder.js
+++ dist/app/utils/QueryBuilder.js
@@ -46,7 +46,7 @@ class QueryBuilder {
     }
     paginate() {
         const page = Number(this.query.page) || 1;
-        const limit = this.query.limit ? Number(this.query.limit) : 0;
+        const limit = this.query.limit ? Number(this.query.limit) : 10;
         const skip = (page - 1) * limit;
         this.modelQuery = this.modelQuery.skip(skip).limit(limit);
         return this;
@@ -58,7 +58,7 @@ class QueryBuilder {
         return __awaiter(this, void 0, void 0, function* () {
             const totalDocuments = yield this.modelQuery.model.countDocuments();
             const page = Number(this.query.page) || 1;
-            const limit = Number(this.query.limit) || 10;
+            const limit = this.query.limit ? Number(this.query.limit) : 10;
             const totalPage = Math.ceil(totalDocuments / limit);
             return { page, limit, total: totalDocuments, totalPage };
         });
